698186e20a67634839dcb30851a937fb96c06567
Thomas Hansen <thomas.hansen@zeitgeist.watch>
2025-01-01 22:10:47 +0100

    Add alembic

--- src/app/main.py
+++ src/app/main.py
@@ -1,16 +1,16 @@
-import sys
-
 from fastapi import FastAPI
 
-from .routers import card_router
-
-
-# if "database.orm" not in sys.modules:
-#     # ensure the orm is started:
-#     import database.orm as orm
-
-#     orm.start_mappers()
-
+from app.routers import card_router
 
 app = FastAPI()
 app.include_router(card_router.router)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    import database.orm as orm
+
+    orm.start_mappers()
+
+    uvicorn.run(app, host="localhost", port=8000)
